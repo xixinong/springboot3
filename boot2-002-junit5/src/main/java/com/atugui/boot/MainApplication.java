ef0006559205d2a2c1498287575e0d5195d125cd
xxnong <xxnong.com>
2023-01-08 14:02:05 +0800

    second commit

--- boot2-002-junit5/src/main/java/com/atugui/boot/MainApplication.java
+++ boot2-002-junit5/src/main/java/com/atugui/boot/MainApplication.java
@@ -2,10 +2,11 @@ package com.atugui.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class,args);
+        ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class, args);
     }
 }
